ad89060e8dddd115ed612f232d6fddb7219ddb19
Tachibanakun9 <167691698+Tachibanakun9@users.noreply.github.com>
2024-07-26 16:49:10 +0530

    uuuuu

---
+++ nancy-next-appp/lib/dbConnect.ts
@@ -0,0 +1,31 @@
+import mongoose from 'mongoose';
+
+type ConnectionObject = {
+  isConnected?: number;
+};
+//write a basic web app template so that i don't have to re write it
+const connection: ConnectionObject = {};
+//if db is connected then we will use that and if not then We will connect it
+async function dbConnect(): Promise<void> {
+  // Check if we have a connection to the database or if it's currently connecting
+  if (connection.isConnected) {
+    console.log('Already connected to the database');
+    return;
+  }
+
+  try {
+    // Attempt to connect to the database
+    const db = await mongoose.connect(process.env.MONGODB_URI || '', {});
+
+    connection.isConnected = db.connections[0].readyState;
+
+    console.log('Database connected successfully');
+  } catch (error) {
+    console.error('Database connection failed:', error);
+
+    // Graceful exit in case of a connection error
+    process.exit(1);
+  }
+}
+
+export default dbConnect;
